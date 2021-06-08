8df74baa4252c6aaff7a2d9f6c62783b9ef581fe
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-06-08 15:47:43 +0200

    [Added] some allowed host

--- config/settings.py
+++ config/settings.py
@@ -24,12 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1',
+    '.herokuapp.com'
+
+]
 
 
 # Application definition
